9d034f761460936458d80caf25e5ff5e9b0d6814
dawa sherpa <sherpadawag1998@gmail.com>
2023-04-05 23:32:40 +0545

    website work for apple company

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Jumbotorn from './components/Jumbotorn';
+import Nav from './components/Nav';
+import SoundSection from './components/soundSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Jumbotorn />
+      <SoundSection />
+    </div>
+  );
+}
+
+export default App;
